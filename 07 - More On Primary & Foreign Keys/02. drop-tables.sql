b6ceb1f0e1f8fe1384258924bb08a4126ada28e0
mashfiq-rayhan <mashfiq.rayhan.ovi@gmail.com>
2024-09-10 00:12:03 +0600

    Day 3 Module 7

---
+++ 07 - More On Primary & Foreign Keys/02. drop-tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE projects_employees;
+DROP TABLE projects;
+DROP TABLE intranet_accounts;
+DROP TABLE employees;
+DROP TABLE teams;
+DROP TABLE company_buildings;
---
+++ 07 - More On Primary & Foreign Keys/04. insert-self-referencing-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (first_name, last_name, supervisor_id)
+VALUES ('Kay', 'Kay', NULL), ('Man', 'Loe', 1), ('Max', 'Mow', 1);
